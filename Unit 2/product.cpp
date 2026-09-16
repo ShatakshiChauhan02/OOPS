0baf16cad4fc6caf3b59108f64629c07afc56f64
ShatakshiChauhan02 <shatakshichauhanabes28@gmail.com>
2026-09-16 09:18:08 +0530

    product

--- Unit 2/product.cpp
+++ Unit 2/product.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std;
-
 class Product {
 private:
     int productId;
@@ -8,9 +7,30 @@ private:
     double price;
 
 public:
-    // Parameterized constructor
     Product(int id, string n, double p) {
         productId = id;
         name = n;
         price = p;
     }
+    Product cmpPrice(const Product &p) {
+        if (price > p.price)
+            return *this;
+        else
+            return p;
+    }
+   
+    void display() {
+        cout << "Product ID: " << productId << endl;
+        cout << "Name: " << name << endl;
+        cout << "Price: " << price << endl;
+    }
+};
+
+int main() {
+    Product p1(101, "Laptop", 55000);
+    Product p2(102, "Smartphone", 40000);
+    Product higher = p1.cmpPrice(p2);
+    cout << "Product with Higher Price:" << endl;
+    higher.display();
+    return 0;
+}
